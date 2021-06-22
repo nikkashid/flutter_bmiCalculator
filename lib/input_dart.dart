9a40e5387dd573ea3e08bb6476a60a9a93b29003
nikhil.kashid1@gmail.com <Shinigami1408>
2021-06-22 15:54:58 +0530

    Creating slider code.

--- lib/input_dart.dart
+++ lib/input_dart.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'constants.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
-
-const bmiCalculator = 'BMI CALCULATOR';
-const bottomContainerHeight = 80.0;
-const activeCardColor = Color(0xFF1D1E33);
-const inactiveCardColor = Color(0xFF111328);
-const bottomContainerColor = Color(0xFFEB1555);
 
 enum Gender { male, female, initial }
 
@@ -19,15 +14,17 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender selectedGender = Gender.initial;
+  int height = 180;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(bmiCalculator),
+        title: Text(kBmiCalculator),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: Row(
@@ -35,8 +32,8 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     selectedGender == Gender.male
-                        ? activeCardColor
-                        : inactiveCardColor,
+                        ? kActiveCardColor
+                        : kInactiveCardColor,
                     IconContent(FontAwesomeIcons.mars, 'MALE'),
                     () {
                       setState(() {
@@ -48,8 +45,8 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     selectedGender == Gender.female
-                        ? activeCardColor
-                        : inactiveCardColor,
+                        ? kActiveCardColor
+                        : kInactiveCardColor,
                     IconContent(FontAwesomeIcons.venus, 'FEMALE'),
                     () {
                       setState(() {
@@ -63,8 +60,35 @@ class _InputPageState extends State<InputPage> {
           ),
           Expanded(
             child: ReusableCard(
-              Color(0xFF1D1E33),
-              new Container(),
+              kActiveCardColor,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Height',
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(height.toString(), style: kNumberTextStyle),
+                      Text('cm', style: kLabelTextStyle)
+                    ],
+                  ),
+                  Slider(
+                      value: height.toDouble(),
+                      min: kMinHeight,
+                      max: kMaxHeight,
+                      activeColor: Color(0xFFEB1555),
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.toInt();
+                        });
+                      })
+                ],
+              ),
               () {},
             ),
           ),
@@ -73,14 +97,14 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: new ReusableCard(
-                    activeCardColor,
+                    kActiveCardColor,
                     new Container(),
                     () {},
                   ),
                 ),
                 Expanded(
                   child: new ReusableCard(
-                    activeCardColor,
+                    kActiveCardColor,
                     new Container(),
                     () {},
                   ),
@@ -89,10 +113,10 @@ class _InputPageState extends State<InputPage> {
             ),
           ),
           Container(
-            color: bottomContainerColor,
+            color: kBottomContainerColor,
             margin: EdgeInsets.only(top: 10.0),
             width: double.infinity,
-            height: bottomContainerHeight,
+            height: kBottomContainerHeight,
           )
         ],
       ),
